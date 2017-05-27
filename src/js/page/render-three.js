3b21475fd24cbc0e5b5533c453280b18840080b1
zhangshuai <1226791563@qq.com>
2017-05-27 17:41:44 +0800

    2017/5/27

--- src/js/page/render-three.js
+++ src/js/page/render-three.js
@@ -1,3 +1,42 @@
 /**
  * Created by matrix on 2017/5/26.
  */
+//搜索框
+$("#search").keyup(function() {
+	if($(this).val() != "") {
+		$(".cancel").show();
+	} else {
+		$(".cancel").hide();
+	}
+});
+$(".cancel").on("click", function() {
+	$("#search").val("");
+	$(this).hide();
+})
+$(".search-btn").on("click", function() {
+	if($("#search").val() != "") {
+		location.href = "news.html";
+	}
+})
+$(document).keydown(function(e) {
+	if($("#search").val() != "") {
+		if(e.keyCode == 13) {
+			location.href = "news.html";
+		}
+	}
+})
+
+//焦点排序
+$(".selected-order").click(function() {
+	if($(".unselected-order").is(":hidden")) {
+		$(".triangle").removeClass().addClass("triangle triangle-up");
+		$(".unselected-order").show();
+	} else {
+		$(".triangle").removeClass().addClass("triangle triangle-down");
+		$(".unselected-order").hide();
+	}
+})
+$(".unselected-order").click(function() {
+	window.location.reload();
+})
+
